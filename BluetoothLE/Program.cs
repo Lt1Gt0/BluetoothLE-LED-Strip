a66cc9d032ffe31036fe4093f5aa39678233fd76
Simon Kaminski <munchkinnm@gmail.com>
2021-09-11 12:39:30 -0700

    Get all bluetoothLE devices and return SP110E

--- BluetoothLE/Program.cs
+++ BluetoothLE/Program.cs
@@ -1,15 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Enumeration;
 
 namespace BluetoothLE
 {
     class Program
     {
+        static DeviceInformation device = null;
         static void Main(string[] args)
         {
+            // Query for extra properties you want returned
+            string[] requestedProperties = { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected" };
+
+            DeviceWatcher deviceWatcher =
+                        DeviceInformation.CreateWatcher(
+                                BluetoothLEDevice.GetDeviceSelectorFromPairingState(false),
+                                requestedProperties,
+                                DeviceInformationKind.AssociationEndpoint);
+
+            // Register event handlers before starting the watcher.
+            // Added, Updated and Removed are required to get all nearby devices
+            deviceWatcher.Added += DeviceWatcher_Added;
+            deviceWatcher.Updated += DeviceWatcher_Updated;
+            deviceWatcher.Removed += DeviceWatcher_Removed;
+
+            // EnumerationCompleted and Stopped are optional to implement.
+            deviceWatcher.EnumerationCompleted += DeviceWatcher_EnumerationCompleted;
+            deviceWatcher.Stopped += DeviceWatcher_Stopped;
+
+            // Start the watcher.
+            deviceWatcher.Start();
+            while (true) {
+                Console.WriteLine("Press Any Key To exit");
+                Console.ReadKey();
+            }
+        }
+
+        private static void DeviceWatcher_Stopped(DeviceWatcher sender, object args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void DeviceWatcher_EnumerationCompleted(DeviceWatcher sender, object args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void DeviceWatcher_Removed(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void DeviceWatcher_Updated(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation args)
+        {
+            Console.WriteLine(args.Name);
+            if(args.Name == "SP110E")
+                device = args;
+            //throw new NotImplementedException();
         }
     }
 }
