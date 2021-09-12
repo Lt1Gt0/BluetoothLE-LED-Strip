2c28f55283c3e6f5512b5ec4ef56e59b433c6713
Simon Kaminski <munchkinnm@gmail.com>
2021-09-12 14:02:51 -0700

    Succesfully subscibe to notificiations from the SP110E module

--- BluetoothLE/Program.cs
+++ BluetoothLE/Program.cs
@@ -4,12 +4,15 @@ using System.Threading.Tasks;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
 using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
 
 namespace BluetoothLE
 {
     class Program
     {
         static DeviceInformation device = null;
+
+        public static string LED_SERVICE_ID = "ffe0";
         static async Task Main(string[] args)
         {
             // Query for extra properties you want returned
@@ -44,14 +47,54 @@ namespace BluetoothLE
                     Console.WriteLine("Press Any Key to pair with SP110E");
                     Console.ReadKey();
                     BluetoothLEDevice bluetoothLEDevice = await BluetoothLEDevice.FromIdAsync(device.Id);
+                    Console.WriteLine("Attempting to pair with SP110E");
 
                     GattDeviceServicesResult result = await bluetoothLEDevice.GetGattServicesAsync();
 
                     if(result.Status == GattCommunicationStatus.Success) {
+                        Console.WriteLine("Pairing succeeded");
                         var services = result.Services;
+                        foreach(var service in services) {
+                            if(service.Uuid.ToString("N").Substring(4,4) == LED_SERVICE_ID) {
+                                Console.WriteLine("Found LED Service");
+
+                                GattCharacteristicsResult characteristicResult = await service.GetCharacteristicsAsync();
+                                if(result.Status == GattCommunicationStatus.Success) {
+                                    var characteristics = characteristicResult.Characteristics;
+                                    foreach(var characteristic in characteristics) {
+                                        Console.WriteLine("------------------");
+                                        Console.Write(characteristic+"\n");
+
+                                        GattCharacteristicProperties properties = characteristic.CharacteristicProperties;
+
+
+                                        if (properties.HasFlag(GattCharacteristicProperties.Notify)) {
+                                            
+                                            Console.WriteLine("Notify Property found");
+                                            GattCommunicationStatus status = await characteristic.WriteClientCharacteristicConfigurationDescriptorAsync(
+                                                GattClientCharacteristicConfigurationDescriptorValue.Notify);
+                                            if(status == GattCommunicationStatus.Success) {
+                                                characteristic.ValueChanged += Characteristic_ValueChanged;
+                                            }
+                                        }
+
+                                    }
+                                }
+                            }
+                         }
                     }
+                    Console.WriteLine("Press Any Key to Exit application");
+                    Console.ReadKey();
+                    break;
                 }
             }
+            deviceWatcher.Stop();
+        }
+
+        private static void Characteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args) {
+            var reader = DataReader.FromBuffer(args.CharacteristicValue);
+            var value = reader.ReadUInt16();
+            Console.WriteLine(value);
         }
 
         private static void DeviceWatcher_Stopped(DeviceWatcher sender, object args)
