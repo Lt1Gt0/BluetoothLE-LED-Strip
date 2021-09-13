bedf4540f8b803ac7fcf83a80531eadc201d61bb
Simon Kaminski <munchkinnm@gmail.com>
2021-09-12 19:50:13 -0700

    sucessful communication with sp110e

---
+++ BluetoothLE/LedStrip.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Phone.System.UserProfile.GameServices.Core;
+
+public class LedStrip {
+    private GattDeviceService Service;
+    private GattCharacteristic IO;
+    private const ushort characteristicID = 0xffe1;
+    public LedStrip(GattDeviceService service) {
+        Service = service;
+        getCharacteristics();
+        initializeIO();
+    }
+
+    private async void getCharacteristics() {
+        foreach (var characteristic in (await Service.GetCharacteristicsAsync()).Characteristics) {
+
+            ushort UUID = BitConverter.ToUInt16(characteristic.Uuid.ToByteArray(), 0);
+            switch (UUID) {
+                case characteristicID:
+                IO = characteristic;
+                break;
+                case 0xffe2:
+                initializeFFE2(characteristic);
+                break;
+            }
+        }
+    }
+
+    private async void initializeIO(){
+        byte[] ffe1Buffer = new byte[] { 0x01, 0xb7, 0xe3, 0xd5 };
+        var ffe1Result = await IO.WriteValueAsync(ffe1Buffer.AsBuffer());
+        if(ffe1Result != GattCommunicationStatus.Success) {
+            throw new Exception("Failed to communicate with SP110E");
+        }
+    }
+    private async void initializeFFE2(GattCharacteristic ffe2){
+        byte[] ffe1Buffer = new byte[] { 0x01, 0x00};
+        var ffe1Result = await ffe2.WriteValueAsync(ffe1Buffer.AsBuffer());
+        if (ffe1Result != GattCommunicationStatus.Success) {
+            throw new Exception("Failed to communicate with SP110E");
+        }
+    }
+
+    public async Task<GattCommunicationStatus> turnOn() {
+        byte[] byteArrayBuffer = new byte[] { 0xfa, 0x0e, 0xc7, 0xaa };
+        return await IO.WriteValueAsync(byteArrayBuffer.AsBuffer());
+    }
+    public async Task<GattCommunicationStatus> turnOff() {
+        byte[] byteArrayBuffer = new byte[] { 0xb0, 0x4f, 0xc2, 0xab };
+        return await IO.WriteValueAsync(byteArrayBuffer.AsBuffer());
+    }
+    public async Task<GattCommunicationStatus> setRGB(byte red, byte green, byte blue) {
+        byte[] byteArrayBuffer = new byte[] { red, green, blue, 0x1e };
+        return await IO.WriteValueAsync(byteArrayBuffer.AsBuffer());
+    }
+    public async Task<GattCommunicationStatus> setPreset(byte preset) {
+        byte[] presetBuffer = new byte[] { preset, 0x00, 0x00, 0x2c };
+        return await IO.WriteValueAsync(presetBuffer.AsBuffer());
+    }
+    public async Task<GattCommunicationStatus> susMode() {    
+        for(int i = 0; i < 255; i+=15) {
+            await setRGB((byte)i, 0x00, 0x00);
+            Thread.Sleep(40);
+        }
+
+        return await setPreset(5);
+    }
+
+}
--- BluetoothLE/Program.cs
+++ BluetoothLE/Program.cs
@@ -6,6 +6,8 @@ using Windows.Devices.Bluetooth.GenericAttributeProfile;
 using Windows.Devices.Enumeration;
 using Windows.Storage.Streams;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace BluetoothLE
 {
@@ -16,6 +18,7 @@ namespace BluetoothLE
         public static string LED_SERVICE_ID = "ffe0";
         public static GattCharacteristic ffe1 = null;
         public static GattCharacteristic ffe2 = null;
+        public static List<LedStrip> ledStrips = new List<LedStrip>();
 
         static async Task Main(string[] args)
         {
@@ -61,44 +64,32 @@ namespace BluetoothLE
 
                                 GattCharacteristicsResult characteristicResult = await service.GetCharacteristicsAsync();
                                 if (result.Status == GattCommunicationStatus.Success) {
-                                    var characteristics = characteristicResult.Characteristics;
-                                    foreach (var characteristic in characteristics) {
-                                        Console.WriteLine("------------------");
-                                        Console.Write(characteristic.Uuid + "\n");
-                                        ushort UUID = BitConverter.ToUInt16(characteristic.Uuid.ToByteArray(), 0);
-                                        switch (UUID) {
-                                            case 0xffe1:
-                                            ffe1 = characteristic;
-                                            break;
-                                            case 0xffe2:
-                                            ffe2 = characteristic;
-                                            break;
-                                        }
-
-                                        GattCharacteristicProperties properties = characteristic.CharacteristicProperties;
-
-                                        if (properties.HasFlag(GattCharacteristicProperties.Notify)) {
-                                            Console.WriteLine("Notify Property found");
-                                        }
-
-                                    }
+                                    LedStrip ledStrip = new LedStrip(service);
+                                    ledStrips.Add(ledStrip);
+                                    await ledStrips[0].setRGB(0xFF, 0xFF, 0xFF);
                                 }
                             }
                         }
                     }
-                    byte[] ffe2Buffer = new byte[] { 0x01, 0x00 };
-                    var ffe2Result = await ffe2.WriteValueAsync(ffe2Buffer.AsBuffer());
-
-                    byte[] ffe1Buffer = new byte[] { 0x01, 0xb7, 0xe3, 0xd5 };
-                    var ffe1Result = await ffe1.WriteValueAsync(ffe1Buffer.AsBuffer());
-                    
-                    Console.WriteLine(ffe2Result);
-                    Console.WriteLine(ffe1Result);
-
-                    byte[] rgbBuffer = new byte[] { 0xFF, 0x00, 0x00, 0x1e };
-                    var rgbResult = await ffe1.WriteValueAsync(rgbBuffer.AsBuffer());
-
-                    Console.WriteLine(rgbResult);
+                    while (true) {
+                        Thread.Sleep(1000);
+                        try {
+                            Process amogus = Process.GetProcessesByName("Among Us")[0];
+                            if(amogus == null) {
+                                continue;
+                            }
+                            foreach(LedStrip led in ledStrips) {
+                                await led.susMode();
+                            }
+                            amogus.WaitForExit();
+                            foreach (LedStrip led in ledStrips) {
+                                await led.setRGB(0xFF, 0xFF, 0xFF);
+                            }
+                        }
+                        catch{
+                            continue;
+                        }
+                    }
 
                     Console.WriteLine("Press Any Key to Exit application");
                     Console.ReadKey();
